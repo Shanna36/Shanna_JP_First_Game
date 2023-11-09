f3721e49e4215adf1a1df5a7d13ad053950b9d09
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-11-09 09:35:40 +0000

    added force to vehicle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //private variables 
-    [SerializeField] private float speed = 20.0f;
+    //[SerializeField] private float speed = 20.0f;
+    [SerializeField] private float horsePower = 0f;
     [SerializeField]  private float turnSpeed = 25.0f;
     private float horizontalInput;
    private float forwardInput;
@@ -14,22 +15,26 @@ public class PlayerController : MonoBehaviour
    public Camera mainCamera;
     public Camera hoodCamera;
     public KeyCode switchKey; 
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+   void Start()
+   {
+    playerRb = GetComponent<Rigidbody>();
+   }
 
     //vehicle moves forward based on delta time
-    void Update()
+    void FixedUpdate() {
+        
+
     {
        //This is where we get player input
        horizontalInput = Input.GetAxis ("Horizontal" + inputId);
        forwardInput = Input.GetAxis ("Vertical" + inputId);
 
         // We move the vehicle forward
-       transform.Translate (Vector3.forward * Time.deltaTime * speed * forwardInput);
+       //transform.Translate (Vector3.forward * Time.deltaTime * speed * forwardInput);
+       playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
        // We Turn the Vehicle 
         transform.Rotate (Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
@@ -38,5 +43,6 @@ public class PlayerController : MonoBehaviour
             mainCamera.enabled = !mainCamera.enabled;
             hoodCamera.enabled = !hoodCamera.enabled;
         }
+    }
     }
 }
